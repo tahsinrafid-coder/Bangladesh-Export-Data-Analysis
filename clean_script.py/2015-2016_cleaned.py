4d0b321c204169e4380b4025e4187e7a40bbaf82
Tahsin Rafid <tahsin.rdl@gmail.com>
2026-06-28 15:03:17 +0600

    Add files via upload

---
+++ clean_script.py/2015-2016_cleaned.py
@@ -0,0 +1,61 @@
+#import pandas and read the csv file, reset the index
+import pandas as pd
+df = pd.read_csv(r"D:\BD_apparel_export_project\Fiscal year wise source file\2015-2016.csv",encoding="latin1", dtype=str)
+df = df.reset_index(drop=True)
+
+# Drop any unnamed columns automatically
+df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+
+
+#Replace the column names with "description" and "value"
+df.columns = ["description","value"]
+
+#create a new column "country" and extract the country code from the "description" column using regex
+df["Country"] = df["description"].where(
+    df["description"].str.match(r"^[A-Z]{2}: ")
+)
+
+#Nan value were replaced with the previous value in the "country" column using forward fill method
+df["Country"] = df["Country"].ffill()
+
+#Remove the country code and the colon from the "Country" column
+df["Country"] = df["Country"].str.replace(
+    r"^[A-Z]{2}: \s*",
+    "",
+    regex=True
+)
+
+#Filter the rows where the "description" column starts with 6 digits followed by a colon and create a copy of the filtered dataframe
+df = df[
+    df["description"].str.match(r"^\d{8}: ", na=False)
+].copy()
+
+#Extract the 6 digit HS code from the "description" column and create a new column "HS_Code"
+df["HS_Code"] = df["description"].str.extract(r"^(\d{6})")
+
+#Remove the 6 digits HS code and the colon from the "description" column and create a new column "Product"
+df["Product"] = df["description"].str.replace(
+    r"^\d{8}:\s*",
+    "",
+    regex=True
+)
+
+#Remove the commas from the "value" column and convert it to numeric type, create a new column "Value"
+df["Value"] = (
+    df["value"]
+    .astype(str)
+    .str.replace(",", "", regex=False)
+)
+
+#Convert the "Value" column to numeric type, coercing errors to NaN
+df["Value"] = pd.to_numeric(df["Value"], errors="coerce")
+
+#Select the relevant columns "Country", "HS_Code", "Product", and "Value" to create the final dataframe
+final_df = df[
+    ["Country", "HS_Code", "Product", "Value"]
+]
+
+final_df.to_csv('2015-2016_cleaned.csv', index=False)   
+
+#Print the first 20 rows of the final dataframe
+print(final_df.head(20))
---
+++ clean_script.py/2019-2020_cleaned.py
@@ -0,0 +1,47 @@
+#import pandas and read the csv file, drop the first 4 rows and reset the index
+import pandas as pd
+df = pd.read_csv(r"D:\BD_apparel_export_project\Fiscal year wise source file\2019-2020.csv",encoding="latin1", dtype=str)
+df = df.drop([0, 1, 2, 3, 4, 5])
+df = df.reset_index(drop=True)
+
+#Replace the column names with "Country", "description", and "Value"
+df.columns = ["Country","description","Value"]    
+
+#Nan value were replaced with the previous value in the "country" column using forward fill method
+df["Country"] = df["Country"].ffill()
+
+#Remove the country code and the colon from the "Country" column
+df["Country"] = df["Country"].str.replace(
+    r"^[A-Z]{2}:\s*",
+    "",
+    regex=True
+)
+
+#Filter the rows where the "description" column starts with 6 digits followed by a colon and create a copy of the filtered dataframe
+df = df[
+    df["description"].str.match(r"^\d{6}:", na=False)
+].copy()
+#Extract the 6 digit HS code from the "description" column and create a new column "HS_Code"
+df["HS_Code"] = df["description"].str.extract(r"^(\d{6})")
+
+#Remove the 6 digits HS code and the colon from the "description" column and create a new column "Product"
+df["Product"] = df["description"].str.replace(
+    r"^\d{6}:\s*",
+    "",
+    regex=True
+)
+
+
+#Select the relevant columns "Country", "HS_Code", "Product", and "Value" to create the final dataframe
+final_df = df[
+    ["Country", "HS_Code", "Product", "Value"]
+]
+
+
+#Remove the commas from the "Value" column and convert it to numeric type, coercing errors to NaN
+df['Value'] = pd.to_numeric(df['Value'].astype(str).str.replace(',', ''), errors='coerce')
+
+final_df.to_csv('2019-2020_cleaned.csv', index=False)
+
+#Print the first 20 rows of the final dataframe
+print(final_df.head(20))
